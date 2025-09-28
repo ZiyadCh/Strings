b59bfaf1f5ebe8a3c14e5361e3cd161788d4251e
ZiyadCh <chetoziyad@gmail.com>
2025-09-28 12:30:59 +0100

    9 fixed

--- ch9.c
+++ ch9.c
@@ -3,13 +3,17 @@
 int main()
 {
   int i =0;
+  int j=0;
   char n[] = "hello world i am u";
   while (n[i] != '\0') {
-    if (n[i] == ' ') {
-      n[i] = n[i+1];
+    if (n[i] != ' ') {
+      n[j] = n[i];
+      j++;
     }
     i++;
   }
+
+  n[j] = '\0';
   printf("%s",n);
 
   return 0;
